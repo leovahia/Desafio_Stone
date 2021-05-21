ea193ae99cb0b686bf5378f0e1568d1274cd0c5e
MatheusYamasato <matheus-yamasato@hotmail.com>
2021-05-21 16:34:07 -0300

    tentando deploy heroku

--- src/config/config.js
+++ src/config/config.js
@@ -2,11 +2,7 @@ require('dotenv').config()
 const { Pool } = require('pg')
 
 const pool = new Pool({
-    user: `${process.env.DB_USER}`,
-    host: `${process.env.DB_HOST}`,
-    database: `${process.env.DB_DATABASE}`,
-    password: `${process.env.DB_PASSWORD}`,
-    port: `${process.env.DB_PORT}`,
+    connectionString: `${process.env.DATABASE_URL}`,
     ssl: {
         rejectUnauthorized: false
     }
